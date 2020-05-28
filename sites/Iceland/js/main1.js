96c89f300474d87c3d91895f8f56c37879f8e87e
Zeleniy_Enot <celerios@mail.ru>
2020-05-28 21:03:41 +0300

    rs v.6

---
+++ sites/Iceland/js/main1.js
@@ -0,0 +1,15 @@
+﻿$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		autoplay: true,
+		dots: true,
+		responsive:{
+			320:{
+				items: 1
+			},
+			426:{
+				items: 3
+			}
+		}
+	});
+});
---
+++ sites/Landscape design/js/main.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$(".owl-carousel").owlCarousel({
+		loop: true,
+		autoplay: true,
+		dots: false,
+		responsive:{
+			0:{
+				items:1
+			}
+		}
+	});
+});
