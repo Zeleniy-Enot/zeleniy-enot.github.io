7820bb67adc377ad3eec72cb28f97d0be904c073
Zeleniy_Enot <celerios@mail.ru>
2020-08-14 12:17:50 +0300

    Сoncrete v.14

--- js/main.js
+++ js/main.js
@@ -48,6 +48,14 @@ $(document).ready(function(){
         $('body,html').animate({scrollTop: top}, 1500);
     });
 });
+$(document).ready(function(){
+    $("#menu2").on("click","a", function (event) {
+        event.preventDefault();
+        var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+        $('body,html').animate({scrollTop: top}, 1500);
+    });
+});
 
 $(function () {
     //script for popups
