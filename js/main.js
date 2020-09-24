faed85f101aaacec71dabdbe3975dc5059b6f86a
Zeleniy-Enot <Zeleniy-Enot@users.noreply.github.com>
2020-09-24 17:55:33 +0300

    crm v.25

--- js/main.js
+++ js/main.js
@@ -79,7 +79,7 @@ jQuery(document).ready(function($) {
 			dots: false,
 			animateOut: 'slideOutDown',
 			animateIn: 'slideInDown',
-			autoplay: true,
+			// autoplay: true,
 			autoplayTimeout: 5000,
 			responsive:{
 				0:{
